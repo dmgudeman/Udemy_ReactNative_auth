b50ea990b17feae1fead39c80ac98ca0c1281012
davidmgudeman <davidmgudeman@gmail.com>
2017-08-30 19:08:39 -0700

    LoginForm: form is done now need to have it connected up

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA4pPNa9aTvDWJA1Wd7VA8-MOzY_m3w9hA',
+      authDomain: 'authentication-aad36.firebaseapp.com',
+      databaseURL: 'https://authentication-aad36.firebaseio.com',
+      projectId: 'authentication-aad36',
+      storageBucket: 'authentication-aad36.appspot.com',
+      messagingSenderId: '110602444455'
+    });
+  }
   render() {
     return (
-       <View>
-         <Header headerText="Authentication" ></Header>
-         <Text>An App!</Text>
-       </View>
-    )
+      <View>
+        <Header headerText="Authentication" />
+        <LoginForm />
+      </View>
+    );
   }
 }
+
 export default App;
